06a9d4527919e4126b27890bdee1594c15b6cb72
Tushar-Maity <tusharmaity989@gmail.com>
2021-09-23 11:16:30 +0530

    React router dom added to navigate to multiple pg

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Nav from './components/Nav';
 import GlobalStyle from './components/GlobalStyle';
 //Importing pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const OurWork = () => {
+    return (
+        <div>
+            <h1 style={{color: "#fff"}}>Our Work</h1>
+        </div>
+    )
+}
+
+export default OurWork
