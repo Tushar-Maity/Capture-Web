b4699cd0c89a7497f56c0d24ae96d645da1a5000
Tushar985 <tusharmaity989@gmail.com>
2021-11-07 13:26:54 +0530

    styling added to the work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -38,10 +38,28 @@ const OurWork = () => {
 
 const Work = styled.div`
     min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+
+    h2 {
+        padding: 1rem 0rem;
+    }
 `;
 
 const Movie = styled.div`
     padding-bottom: 10rem;
+
+    .line {
+        height: 0.5rem;
+        background: #ccc;
+        margin-bottom: 3rem;
+    }
+
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
 `;
 
 export default OurWork
